e596e042d7ba96a6c22d8c1d6476a7085f3a28b5
HG570 <rafaelpinheirovgp@gmail.com>
2024-08-31 23:56:20 -0300

    feat: add bike rack repository

---
+++ src/main/java/br/gov/sp/cptm/bykerack/data/respository/BikeRackRepository.java
@@ -0,0 +1,7 @@
+package br.gov.sp.cptm.bykerack.data.respository;
+
+import br.gov.sp.cptm.bykerack.data.model.BikeRack;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BikeRackRepository extends JpaRepository<BikeRack, Long> {
+}
